6c02ff5a7087390f50b840b4c7f91cededcc227c
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2025-12-26 23:18:09 +0530

    kth-largest

---
+++ kthLargestNumber.java
@@ -0,0 +1,29 @@
+
+
+public class kthLargestNumber {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,5,6,4};
+        int k = 2;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int num : arr){
+            if(num<min) min = num;
+            if(num>max) max = num;
+        }
+        int[] freq = new int[max-min+1];
+        
+        for(int num:arr){
+            freq[num-min]++;
+        }
+        int result = -1;
+        for(int i=freq.length-1;i>=0;i--){
+            k-=freq[i];
+            if(k<=0){
+                result=i+min;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
