08f7a6a5587a8dd9dbdf9e23c134dc77dc27b3f9
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2025-12-31 22:17:07 +0530

    sorted

---
+++ SortedSquares.java
@@ -0,0 +1,29 @@
+public class SortedSquares {
+
+    public static int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+
+        int[] result = sortedSquares(nums);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
