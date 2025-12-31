c3a26096a4ef14327927b04f6169d0fedabf008a
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2025-12-31 22:13:23 +0530

    searchPosition

---
+++ SearchInsertPosition.java
@@ -0,0 +1,27 @@
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+
+        int result = searchInsert(nums, target);
+        System.out.println("Index: " + result);
+    }
+}
